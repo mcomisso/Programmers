a56929f8934b875a7835ae3ab9191eda48b82814
Matteo Comisso <mcomisso@rmn.com>
2018-03-22 16:42:56 +0000

    Inject viewcontroller in appdelegate

--- CleanArch/AppDelegate.swift
+++ CleanArch/AppDelegate.swift
@@ -13,9 +13,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        func injectVC(_ vc: UIViewController) {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = vc
+            window?.makeKeyAndVisible()
+        }
+
+
+        let storyboardName = "Main"
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+
+        if let viewController = storyboard.instantiateInitialViewController() as? UINavigationController {
+
+            let programmersViewController = viewController.topViewController as! ProgrammersListTableViewController
+
+            let gateway = ProgrammersRepository()
+            let useCase = ShowProgrammerListUseCase.init(entityGateway: gateway)
+            let presenter = ProgrammerListPresenter(useCase: useCase)
+            useCase.presenter = presenter // Connect backwards
+
+            programmersViewController.presenter = presenter
+
+            injectVC(viewController)
+
+        }
+
+
         return true
     }
 
