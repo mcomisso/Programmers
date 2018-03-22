9f4dcc03d6f3b369b8284aa40510ed577b594111
Matteo Comisso <mcomisso@rmn.com>
2018-03-22 15:22:59 +0000

    Add a presenter for ProgrammerListPresentation

---
+++ CleanArch/Presenter/ProgrammerListPresenter.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammerListPresenter.swift
+//  CleanArch
+//
+//  Created by Matteo Comisso on 22/03/2018.
+//  Copyright © 2018 ReatailMeNot UK Ltd. All rights reserved.
+//
+
+import Foundation
+
+class ProgrammerListPresenter: ProgrammerListPresentation {
+
+    func present(programmers: [ProgrammerResponse]) {
+
+    }
+
+}
