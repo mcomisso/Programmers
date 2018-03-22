3551ee0a9d63661a98df63c37deb11e03fde3a74
Matteo Comisso <mcomisso@rmn.com>
2018-03-22 15:31:44 +0000

    Create a View and its UITableViewController subclass.

---
+++ CleanArch/ProgrammersListTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProgrammersListTableViewController.swift
+//  CleanArch
+//
+//  Created by Matteo Comisso on 22/03/2018.
+//  Copyright © 2018 ReatailMeNot UK Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ProgrammersListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
