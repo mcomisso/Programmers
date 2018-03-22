a09119fa37eb8b94e5a22e7472909ef7f5a838d4
Matteo Comisso <mcomisso@rmn.com>
2018-03-22 14:23:53 +0000

    Initial Commit

---
+++ CleanArch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanArch
+//
+//  Created by Matteo Comisso on 22/03/2018.
+//  Copyright © 2018 ReatailMeNot UK Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
